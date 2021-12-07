7363ed24c88cbb7ff400fd551049a6cc1241acde
h4x0rlol <mazafaker.com@gmail.com>
2021-12-07 23:58:54 +0600

    fix: add weather icons

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -34,34 +34,74 @@ export const Weather = () => {
                 </svg>
                 Omsk, RU
               </p>
+              {/* 
+              <svg
+                className="h-12 w-12 pb-2 ml-6"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                <path d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72" />
+                <path
+                  d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                  transform="rotate(60 12 12)"
+                />
+                <path
+                  d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                  transform="rotate(120 12 12)"
+                />
+                <path
+                  d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                  transform="rotate(180 12 12)"
+                />
+                <path
+                  d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                  transform="rotate(240 12 12)"
+                />
+                <path
+                  d="M10 4l2 1l2 -1m-2 -2v6.5l3 1.72"
+                  transform="rotate(300 12 12)"
+                />
+              </svg> */}
 
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12 ml-6"
-                fill="none"
+                class="h-12 w-12 pb-2 ml-6"
                 viewBox="0 0 24 24"
+                fill="none"
                 stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
+                <circle cx="12" cy="12" r="5" />{" "}
+                <line x1="12" y1="1" x2="12" y2="3" />{" "}
+                <line x1="12" y1="21" x2="12" y2="23" />{" "}
+                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                <line x1="1" y1="12" x2="3" y2="12" />{" "}
+                <line x1="21" y1="12" x2="23" y2="12" />{" "}
+                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
               </svg>
             </div>
           </div>
 
           <div>
-            <div className="mb-10 ml-28 w-max">
-              <h2 className=" font-semibold text-3xl leading-none pb-1">
+            <div className="mb-6 ml-28 lg:w-max">
+              <h2 className="font-semibold text-3xl leading-none pb-1">
                 Scattered clouds
               </h2>
-              <h3 className="font font-medium">Temp: +29 ºC</h3>
-              <h3 className="">Feels like: +29 ºC</h3>
-              <h3 className="">Humidity: 70 %</h3>
-              <h3 className="">Pressure: 1032</h3>
-              <h3 className="">Wind speed: 3.13</h3>
+              <h3 className="font-medium">Temp: +29 ºC</h3>
+              <h3>Feels like: +29 ºC</h3>
+              <h3>Humidity: 70 %</h3>
+              <h3>Pressure: 1032</h3>
+              <h3>Wind speed: 3.13</h3>
             </div>
           </div>
         </div>
